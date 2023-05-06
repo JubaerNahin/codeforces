a14f7a804a23ecd8df3061209e30fddb28ad8f6c
Nahin <jubaernahin17@gmail.com>
2023-05-06 11:34:37 +0600

    init

--- 723A.cpp
+++ 723A.cpp
@@ -1,12 +1,12 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int Max,Min,Ma,mid,Mi,x1,x2,x3;
-    cin>>x1>>x2>>x3;
-    Max= max(x1,x2);
-    Min= min(x1,x2);
-    Ma= max(Max,x3);
-    Mi= min(Min,x3);
-    mid= max(Min,x3);
-    cout<<(Ma-mid)+(mid-Mi);
+
+int main() 
+{
+    int x[3];
+    for(int i=0;i<3;i++){
+      cin>>x[i];
+    }
+    sort(x,x+3);
+    cout<<(x[1]-x[0])+(x[2]-x[1]);
 }
